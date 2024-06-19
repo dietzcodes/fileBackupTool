5513810c88e7e777fc95c1c4fcdd52c8ff9f2862
Carter Dietz <cjdietz09@protonmail.com>
2024-06-19 12:18:36 -0500

    "added destination folder button"

--- main.py
+++ main.py
@@ -19,9 +19,11 @@ class Window(QMainWindow):
             #create widgets
             self.backupButton = QPushButton("Backup Files") #backup button widget
             self.addFolderButton = QPushButton("Add Folder") #add folder widget
+            self.destFolderButton = QPushButton("Set Destination Folder")
             self.folderListWidget = QLabel("No folders yet")
             #add widgets to layout
             self.layout.addWidget(self.addFolderButton)
+            self.layout.addWidget(self.destFolderButton)
             self.layout.addWidget(self.backupButton)
             self.layout.addWidget(self.folderListWidget, alignment=Qt.AlignmentFlag.AlignLeft)
             #add the layout to the window
@@ -30,6 +32,7 @@ class Window(QMainWindow):
             self.layout.setSpacing(0)            
             #connect button to event handler
             self.addFolderButton.clicked.connect(self.folderDialogClickHandler)
+            self.destFolderButton.clicked.connect(self.destDialogClickHandler)
 
             self.layout.setSpacing(1)
       def folderDialogClickHandler(self):
@@ -53,6 +56,16 @@ class Window(QMainWindow):
                   self.layout.addWidget(self.folderListWidget)
 
                   print(self.selectedFolders)
+      def destDialogClickHandler(self):
+            self.destFolder = ""
+            destDialog = QFileDialog()
+            destDialog.setFileMode(QFileDialog.FileMode.Directory)
+            destSuccessful = destDialog.exec()
+            
+            if destSuccessful:
+                  self.destFolder = str(destDialog.selectedFiles())[1:-1]
+                  self.destFolderButton.setText(self.destFolder)
+                  print(self.destFolder)
 window = Window()
 window.show()
 app.exec()
